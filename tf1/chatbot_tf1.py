5cae120b98fe9b86bc6a6c7cb5c6c7fcd6e6f091
JungSeob Lee <60927808+js-lee-AI@users.noreply.github.com>
2020-11-06 04:29:03 +0900

    update chatbot_tf1.py

--- tf1/chatbot_tf1.py
+++ tf1/chatbot_tf1.py
@@ -27,18 +27,13 @@ def interact_model(
     with open(os.path.join('./models', '345M_org', 'hparams.json')) as f:
         hparams.override_from_dict(json.load(f))
 
-    if length is None:
-        length = hparams.n_ctx // 2
-    elif length > hparams.n_ctx:
-        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
-
     with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
+        contxt = tf.placeholder(tf.int32, [batch_size, None])
         np.random.seed(seed)
         tf.set_random_seed(seed)
         output = sample.sample_sequence(
             hparams=hparams, length=length,
-            context=context,
+            context=contxt,
             batch_size=batch_size,
             temperature=temperature, top_k=top_k, top_p=top_p
         )
@@ -53,17 +48,13 @@ def interact_model(
             utterances = input("user: ")
             raw_text +='\n' + 'user: '+ utterances  + '\n' + 'bot: '
 
-            context_tokens = enc.encode(raw_text)
-            generated = 0
+            contxt_tokens = enc.encode(raw_text)
             for _ in range(nsamples // batch_size):
                 out = sess.run(output, feed_dict={
-                    context: [context_tokens for _ in range(batch_size)]
-                })[:, len(context_tokens):]
+                    contxt: [contxt_tokens for _ in range(batch_size)]
+                })[:, len(contxt_tokens):]
                 for i in range(batch_size):
-                    generated += 1
                     text = enc.decode(out[i])
                     result=list(text.partition('\n'))
                     print('bot:' + result[0])
                     raw_text += str(result[0])
-
-
